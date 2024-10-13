5a627b0dfab83670f115f413db99edcc2593fc78
ben <ben@bnsmcx.com>
2024-10-13 03:36:27 +0000

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module yvpn
+
+go 1.21.11
---
+++ shell.nix
@@ -0,0 +1,28 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    go
+    git
+    gopls
+    lunarvim
+    zellij
+  ];
+
+  shellHook = ''
+    echo "Welcome to your nix-shell environment!"
+    
+    # Setting aliases
+    alias vim=lvim
+    
+    # Launch zellij using zsh if it is installed
+    if type zsh > /dev/null 2>&1; then
+        zellij --layout layout.kdl options --simplified-ui true --default-shell zsh
+    else
+        zellij --layout layout.kdl options --simplified-ui true
+    fi
+  '';
+}
