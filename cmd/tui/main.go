5da3f451a2832b9fefc14b0e08af7612903ec33c
ben <ben@pop-os.localdomain>
2024-10-20 07:00:16 +0000

    Go straight to dash if tokens in env

--- cmd/tui/main.go
+++ cmd/tui/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
@@ -11,8 +12,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	p := tea.NewProgram(NewOnboarding(), tea.WithAltScreen())
-	if _, err := p.Run(); err != nil {
-		log.Fatal(err)
+	do, good1 := os.LookupEnv("DIGITAL_OCEAN_TOKEN")
+	ts, good2 := os.LookupEnv("TAILSCALE_API")
+	if good1 && good2 {
+		p := tea.NewProgram(NewDash(do, ts), tea.WithAltScreen())
+		if _, err := p.Run(); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		p := tea.NewProgram(NewOnboarding(), tea.WithAltScreen())
+		if _, err := p.Run(); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
