9f4786349bc0774b86f7e7303cb291117c420045
ben <ben@pop-os.localdomain>
2024-12-02 09:57:01 +0000

    Tweak huh theme and dimensions

--- cmd/tui/onboard.go
+++ cmd/tui/onboard.go
@@ -58,14 +58,14 @@ func (m Onboard) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m Onboard) View() string {
 	top := m.getTopBar()
 	content := lipgloss.Place(m.width, m.height-lipgloss.Height(top),
-		lipgloss.Center, lipgloss.Center, m.form.View())
+		lipgloss.Center, lipgloss.Center, m.getStyledForm())
 	return fmt.Sprint(lipgloss.JoinVertical(lipgloss.Center, top, content))
 }
 
 func (m Onboard) getTopBar() string {
 	style := lipgloss.NewStyle().
-		Background(lipgloss.Color("9")).
-		Foreground(lipgloss.Color("15")).
+		Background(lipgloss.Color("#E59500")).
+		Foreground(lipgloss.Color("0")).
 		MarginBottom(1)
 	left := lipgloss.NewStyle().Align(lipgloss.Left).PaddingLeft(1).
 		Render("Onboarding")
@@ -75,6 +75,62 @@ func (m Onboard) getTopBar() string {
 		m.width-(lipgloss.Width(left)+lipgloss.Width(right)))
 	bar := lipgloss.JoinHorizontal(lipgloss.Center, left, padding, right)
 	return style.Render(bar)
+}
+
+func (m Onboard) getStyledForm() string {
+	m.form.WithTheme(yvpnThem()).WithWidth(m.width - (m.width / 4))
+	return lipgloss.NewStyle().
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("#E59500")).
+		PaddingTop(2).
+		PaddingLeft(2).
+		PaddingRight(2).
+		Render(m.form.View())
+}
+
+func yvpnThem() *huh.Theme {
+	t := huh.ThemeBase()
+	var (
+		background = lipgloss.AdaptiveColor{Dark: "#282a36"}
+		selection  = lipgloss.AdaptiveColor{Dark: "#44475a"}
+		foreground = lipgloss.AdaptiveColor{Dark: "#f8f8f2"}
+		comment    = lipgloss.AdaptiveColor{Dark: "#6272a4"}
+		green      = lipgloss.AdaptiveColor{Dark: "#50fa7b"}
+		prompt     = lipgloss.AdaptiveColor{Dark: "#E59500"}
+		red        = lipgloss.AdaptiveColor{Dark: "#ff5555"}
+		yellow     = lipgloss.AdaptiveColor{Dark: "#f1fa8c"}
+	)
+
+	t.Focused.Base = t.Focused.Base.BorderForeground(selection)
+	t.Focused.Title = t.Focused.Title.Foreground(prompt)
+	t.Focused.NoteTitle = t.Focused.NoteTitle.Foreground(prompt)
+	t.Focused.Description = t.Focused.Description.Foreground(comment)
+	t.Focused.ErrorIndicator = t.Focused.ErrorIndicator.Foreground(red)
+	t.Focused.Directory = t.Focused.Directory.Foreground(prompt)
+	t.Focused.File = t.Focused.File.Foreground(foreground)
+	t.Focused.ErrorMessage = t.Focused.ErrorMessage.Foreground(red)
+	t.Focused.SelectSelector = t.Focused.SelectSelector.Foreground(yellow)
+	t.Focused.NextIndicator = t.Focused.NextIndicator.Foreground(yellow)
+	t.Focused.PrevIndicator = t.Focused.PrevIndicator.Foreground(yellow)
+	t.Focused.Option = t.Focused.Option.Foreground(foreground)
+	t.Focused.MultiSelectSelector = t.Focused.MultiSelectSelector.Foreground(yellow)
+	t.Focused.SelectedOption = t.Focused.SelectedOption.Foreground(green)
+	t.Focused.SelectedPrefix = t.Focused.SelectedPrefix.Foreground(green)
+	t.Focused.UnselectedOption = t.Focused.UnselectedOption.Foreground(foreground)
+	t.Focused.UnselectedPrefix = t.Focused.UnselectedPrefix.Foreground(comment)
+	t.Focused.FocusedButton = t.Focused.FocusedButton.Foreground(yellow).Background(prompt).Bold(true)
+	t.Focused.BlurredButton = t.Focused.BlurredButton.Foreground(foreground).Background(background)
+
+	t.Focused.TextInput.Cursor = t.Focused.TextInput.Cursor.Foreground(yellow)
+	t.Focused.TextInput.Placeholder = t.Focused.TextInput.Placeholder.Foreground(comment)
+	t.Focused.TextInput.Prompt = t.Focused.TextInput.Prompt.Foreground(yellow)
+
+	t.Blurred = t.Focused
+	t.Blurred.Base = t.Blurred.Base.BorderStyle(lipgloss.HiddenBorder())
+	t.Blurred.NextIndicator = lipgloss.NewStyle()
+	t.Blurred.PrevIndicator = lipgloss.NewStyle()
+
+	return t
 }
 
 func NewOnboarding() Onboard {
